32c6a03de6695d6995b144314d8e0ce7e481041d
TIM MORENCY <tmorency@madisoncollege.edu>
2016-09-15 12:43:06 -0600

    adding more to the original doa

--- src/main/java/com/tmorency/edu/controller/roomMateDao.java
+++ src/main/java/com/tmorency/edu/controller/roomMateDao.java
@@ -3,7 +3,9 @@ package com.tmorency.edu.controller;
 import com.tmorency.edu.entity.roomMate;
 import com.tmorency.edu.persistence.SessionFactoryProvider;
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,5 +22,31 @@ public class roomMateDao {
         Session session = SessionFactoryProvider.getSessionFactory().openSession();
         rms = session.createCriteria(roomMate.class).list();
         return rms;
+    }
+
+    public roomMate getRoomMate(int id) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        roomMate rm = (roomMate) session.get(roomMate.class, id);
+        return rm;
+    }
+
+    public roomMate insertNewRoomMate(String firstName, String lastName, String email, String phoneNumber) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        session.beginTransaction();
+        roomMate rmNew = new roomMate();
+        rmNew.setEmail(email);
+        rmNew.setFirstName(firstName);
+        rmNew.setLastName(lastName);
+        rmNew.setPhoneNumber(phoneNumber);
+        session.save(rmNew);
+        session.getTransaction().commit();
+        return rmNew;
+    }
+
+    public List<roomMate> searchRoomMates(String fieldName, String searchVal) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Criteria criteria = session.createCriteria(roomMate.class);
+        criteria.add(Restrictions.eq(fieldName, searchVal));
+        return criteria.list();
     }
 }
--- src/test/java/com/tmorency/eduTest/entity/roomMateDaoTest.java
+++ src/test/java/com/tmorency/eduTest/entity/roomMateDaoTest.java
@@ -5,8 +5,10 @@ import com.tmorency.edu.entity.roomMate;
 import org.apache.log4j.Logger;
 import org.junit.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
@@ -28,4 +30,39 @@ public class roomMateDaoTest {
         assertTrue(checker);
 
     }
+
+    @Test
+    public void testInsert() {
+        roomMateDao rmd = new roomMateDao();
+        roomMate rm = rmd.insertNewRoomMate("TestFirst", "testLast", "test@test.com","608-123-4567");
+        logger.info(rm);
+        assertEquals("TestFirst", rm.getFirstName());
+
+        int checker = 0;
+        if(rm.getId() > 0) {
+            checker = rm.getId();
+        }
+        assertTrue(checker > 0);
+    }
+
+    @Test
+    public void getSingleRoomMate() {
+        int id = 1;
+
+        roomMateDao rmd = new roomMateDao();
+        roomMate rm = rmd.getRoomMate(id);
+        logger.info(rm);
+        assertTrue(rm.getFirstName().length() > 0);
+    }
+
+    @Test
+    public void testSeach() {
+        roomMateDao rmd = new roomMateDao();
+        List<roomMate> rms = new ArrayList<roomMate>();
+        rms = rmd.searchRoomMates("firstName", "Tim");
+        logger.info(rms);
+        assertTrue(rms.size() > 0);
+    }
 }
+
+
